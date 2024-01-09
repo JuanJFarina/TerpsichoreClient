31955ce085d0887b7652523433ad55bb2e87d6d3
camila <camilamaita07@gmail.com>
2024-01-09 20:01:32 -0300

    validators register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { AuthComponent } from './modules/auth/auth.component';
+import { RegistrationComponent } from './modules/registration/registration.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,9 @@ export const routes: Routes = [
 
     {
         path: "login", component: LoginComponent
+    },
 
+    {
+        path: "registration", component: RegistrationComponent
     }
 ];
