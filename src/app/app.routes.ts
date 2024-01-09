60b2797b2f06624241089017d413786653119917
camila <camilamaita07@gmail.com>
2024-01-08 23:07:17 -0300

    added tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        
+        path: "login", component: LoginComponent,
+
+    }
+];
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(
 
+  ) { }
+
+  loginForm = new FormGroup({
+    username: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(10)
+    ]),
+    password: new FormControl<string>('',
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(15)
+      ])
+
+  })
 }
