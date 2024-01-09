2dd4e894a13a44fb56804515949f19f8f1d08e30
camila <camilamaita07@gmail.com>
2024-01-08 23:22:38 -0300

    created auth.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
+import { AuthComponent } from './modules/auth/auth.component';
 
 export const routes: Routes = [
     {
-        
-        path: "login", component: LoginComponent,
+        path: "", component: AuthComponent
+    },
+
+    {
+        path: "login", component: LoginComponent
 
     }
 ];
