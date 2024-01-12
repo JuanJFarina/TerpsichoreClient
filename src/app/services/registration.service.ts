660666581a95f5748fe8d9ad56a2a4f38b10eef7
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2024-01-12 00:28:02 -0300

    add: regsitration servcie

---
+++ src/app/services/registration.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  // #http = inject(HttpClient);
+
+  // register(data: User) {
+    // return // data
+  // }
+
+  constructor() { }
+}
